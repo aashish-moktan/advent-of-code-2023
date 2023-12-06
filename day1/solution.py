856e3de463575dffc142388efb16a412a6ec75e1
twobits <twobits@pop-os.localdomain>
2023-12-06 22:35:59 +0545

    Done with day1

--- day1/solution.py
+++ day1/solution.py
@@ -1,27 +1,34 @@
 import re
-def puzzle_two():
-    regex = r"(?=(zero|one|two|three|four|five|six|seven|eight|nine|[0-9]))"
-    number_dict = {
-        "zero": "0",
-        "one": "1",
-        "two": "2",
-        "three": "3",
-        "four": "4",
-        "five": "5",
-        "six": "6",
-        "seven": "7",
-        "eight": "8",
-        "nine": "9",
-    }
-    result_sum = 0
-    with open("input.txt", "r", encoding="utf_8") as input_file:
-        for line in input_file:
-            number_list = [
-                number_dict[num] if num in number_dict else num
-                for num in re.findall(regex, line)
-            ]
-            number = int(number_list[0] + number_list[-1])
-            result_sum += number
-    return result_sum
 
-print(puzzle_two())
+numbers = ("one", "two", "three", "four", "five", "six", "seven", "eight", "nine")
+digits = ("1", "2", "3", "4", "5", "6", "7", "8", "9")
+
+def get_pos(line):
+    nums = []
+    for i in range(len(numbers)):
+        pos = line.find(numbers[i])
+        indices = [match.start() for match in re.finditer(numbers[i], line)]
+        for z in indices:
+            if (z >= 0):
+                nums.append((z, i + 1))
+
+    for i in range(len(digits)):
+        pos = line.find(digits[i])
+        indices = [match.start() for match in re.finditer(digits[i], line)]
+        for z in indices:
+            if (z >= 0):
+                nums.append((z, i + 1))
+
+    return ((min(nums, key=lambda x: x[0]), max(nums, key=lambda x: x[0])))
+
+def get_num(line):
+    pos = get_pos(line)
+    print("Position = ", pos)
+    return (str(pos[0][1])+str(pos[1][1]))
+
+sum = 0
+with open("example_input.txt") as f:
+    for line in f:
+        sum += int(get_num(line))
+
+print(sum)
